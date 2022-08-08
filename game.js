49469b5f675a96e3600afb9727109b482ffb5e74
gregorop <uegorigor@gmail.com>
2022-08-09 02:17:58 +0300

    высота ширина экрана, герой - мячик

--- game.js
+++ game.js
@@ -1,5 +1,9 @@
 
+//сразу после загрузки страницы срабатывает этот код
 document.addEventListener("DOMContentLoaded", function() {
+
+    W = document.activeElement.clientWidth
+    H = document.activeElement.clientHeight
 
     var hero = document.getElementsByClassName("hero")[0]
     

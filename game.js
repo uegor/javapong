2ba5822a094c0c0765bcc81274fdf24d2ccab12b
gregorop <uegorigor@gmail.com>
2022-08-09 04:15:32 +0300

    мячик летает и стукается

--- game.js
+++ game.js
@@ -1,6 +1,14 @@
 let x,y,hero, player1, player2
 
-let xspeed = 0, yspeed = 0
+//Math.random() дает от 0 до 1
+//round округляет до целого
+//xspeed - направление слево вправо, должно быть строго
+let xspeed = Math.round(Math.random())
+if (xspeed==0) xspeed = -1
+
+//направление вверх-вниз если будет близко к -1 или 1, то мяч будет улетать
+//для этого делим
+let yspeed = Math.random()/2
 
 let player1_speed = 0, player2_speed = 0
 let player1_y, player2_y
@@ -40,6 +48,11 @@ function change_speed(e){
 function move(){
     x += xspeed
     y += yspeed
+
+    //x и у это координаты левого верхнего угла мячика
+    if (x<0 || x>W-50) xspeed *= -1
+    if (y<0 || y>H-50) yspeed *= -1
+
     hero.style.left = x 
     hero.style.top = y
 
